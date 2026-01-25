2e565c6c580f7dc4489fb1089d6d9f3231875397
ffbase000-cyber <ffbase000@gmail.com>
2026-01-25 19:32:08 +0300

    Update payments.js

--- backend/routes/payments.js
+++ backend/routes/payments.js
@@ -83,7 +83,7 @@ router.post('/pay', async (req, res) => {
         tx_ref,
         email: buyerEmail,
         first_name: buyerFullName,
-        callback_url: `https://adama-shopping-20ij.onrender.com/payment-success.html?orderId=${orderId}&tx_ref=${tx_ref}`
+        callback_url: `https://adama-shopping-19bf.onrender.com/payment-success.html?orderId=${orderId}&tx_ref=${tx_ref}`
       })
     });
 
